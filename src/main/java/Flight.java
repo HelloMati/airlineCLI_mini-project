2da070dae382e1443bd86310f2a09dd2a0ba1b10
Mohamed <mohamed@Mohameds-MacBook-Pro.local>
2023-02-06 17:10:06 +0000

    added some base code to continue on from in Flight

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,48 @@
 public class Flight {
+
+    String destination;
+    int FlightId;
+    ArrayList<Passenger> passengers;
+
+    public Flight (String destination) {
+        this.destination = destination;
+        this.passengers = new ArrayList<>();
+        this.FlightId = flightid; // we need to add the flight number generator here
+    }
+
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getFlightId() {
+        return FlightId;
+    }
+
+    public ArrayList<Passenger> getPassengers() {
+        return passengers;
+    }
+
+
+
+    // use todays scannner lesson to find passengers and add/remove them - figure out the right coding
+
+    public void addPassengerToFlight(Passenger passenger){
+        try{
+            if ()
+        }
+    }
+
+
+   public void removePassengerFromFlight(Passenger passenger){
+        try{
+            if()
+        }
+   }
+
+
 }
