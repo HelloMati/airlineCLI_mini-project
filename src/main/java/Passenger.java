6af54783cd2d900d45ae0050c431e36bdf60bf5c
Mohamed <mohamed@Mohameds-MacBook-Pro.local>
2023-02-06 16:46:14 +0000

    added more to passengers

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -3,4 +3,34 @@ public class Passenger {
         private String name;
         private int contactInfo;
         private int Id;
+
+        public Passenger(String name,int contactInfo, int id){
+                this.name = name;
+                this.contactInfo = contactInfo;
+                this.Id = id;
+        }
+
+        public int getId(){
+                return Id;
+        }
+
+        public String getName(){
+                return name;
+        }
+
+        public void setId(int id) {
+                this.Id = id;
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
 }
